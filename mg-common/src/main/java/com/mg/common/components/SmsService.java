79db7b962701c05dd2ea25d1eb93978f768663db
liukefu2050 <liukefu2050@sina.com>
2020-07-22 15:12:28 +0800

    增加短信签名

--- mg-common/src/main/java/com/mg/common/components/SmsService.java
+++ mg-common/src/main/java/com/mg/common/components/SmsService.java
@@ -127,6 +127,7 @@ public class SmsService {
 		//模板代码
 		String accessKeyId = PropertyConfigurer.getConfig("sms.accessKeyId");
 		String accessKeySecret = PropertyConfigurer.getConfig("sms.accessKeySecret");
+		String signName = PropertyConfigurer.getConfig("sms.signName");
 		//初始化acsClient,暂不支持region化
 		IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
 		DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
@@ -137,7 +138,11 @@ public class SmsService {
 		//必填:待发送手机号
 		request.setPhoneNumbers(mobile);
 		//必填:短信签名-可在短信控制台中找到
-		request.setSignName("众家小店");
+		if(StringUtils.isNotBlank(signName)){
+			request.setSignName(signName);
+		}else{
+			request.setSignName("秀上");
+		}
 		//必填:短信模板-可在短信控制台中找到
 		request.setTemplateCode(templateCode);
 		//可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
