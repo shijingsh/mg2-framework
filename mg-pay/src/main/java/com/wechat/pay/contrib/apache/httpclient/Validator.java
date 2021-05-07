8f303e9a8bbf96a933c9f2df90b4c7a298e9828f
liukefu2050 <liukefu2050@sina.com>
2021-05-07 18:13:57 +0800

    mg-pay

--- mg-pay/src/main/java/com/wechat/pay/contrib/apache/httpclient/Validator.java
+++
@@ -1,8 +0,0 @@
-package com.wechat.pay.contrib.apache.httpclient;
-
-import java.io.IOException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-
-public interface Validator {
-  boolean validate(CloseableHttpResponse response) throws IOException;
-}
